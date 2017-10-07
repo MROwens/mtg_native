fc46095a547ed93a6dd4f508d2e1fe22828cf24c
Matt Owens <Wulfy65@gmail.com>
2017-10-07 19:34:38 -0400

    Fixed image display issue

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Login from './src/Login';
 import Search from './src/Search';
 import Deck from './src/components/Deck';
 import Decklist from './src/Decklist';
+import DeckCreation from './src/DeckCreation';
 
 export default class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends React.Component {
        //<Login/>
       //<Search/>
       <Decklist/>
+      //<DeckCreation />
     );
   }
 }
--- src/DeckCreation.js
+++ src/DeckCreation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, FlatList } from 'react-native';
 import Header from './components/Header';
 import Deck from './components/Deck';
 
@@ -8,11 +8,15 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Image style={styles.img} source={require('../assets/img/app_background.png')} />
-        <View>
+        <View style={styles.header}>
           <Header />
+          <Deck/>
         </View>
         <View style={styles.decklist}>
-          <Deck/>
+          <FlatList
+            data={[{key: 'Card Name1'}, {key: 'Card Name2'}, {key: 'Card Name3'}, {key: 'Card Name4'}]}
+            renderItem={({item}) => <Text>{item.key}</Text>}
+          />
         </View>
       </View>
     );
@@ -23,6 +27,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header: {
+    flex: 1
+  },
   img: {
     flex: 1,
     position: 'absolute',
@@ -31,7 +38,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   decklist: {
-    flex: 1,
+    flex: 3,
     marginTop: 40
   }
 });
