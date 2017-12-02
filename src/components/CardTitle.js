df98ba8207a36b8c48a40ff1acd66b2463416e6c
Matt Owens <Wulfy65@gmail.com>
2017-12-01 23:26:18 -0500

    new basic components

---
+++ src/components/CardTitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+export default class CardTitle extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
